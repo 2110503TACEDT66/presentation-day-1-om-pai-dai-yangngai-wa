3fa9b1816e2cf258169d077ca6eec6fc2d7d4aaf
night-sornram <136814474+night-sornram@users.noreply.github.com>
2024-03-04 19:18:31 +0700

    change status 200 to 201

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -93,7 +93,7 @@ exports.addAppointment=async (req,res,next)=>{
 
         const appointment = await Appointment.create(req.body);
 
-        res.status(200).json({
+        res.status(201).json({
             success:true,
             data: appointment
         });
